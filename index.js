d3a4f8c7653d5149b88940500999057135aac9a2
Massik <samosh000@gmail.com>
2019-01-16 20:33:59 +0300

    'initial'

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+const fs =require('fs');
+console.log('Hello innerWorld');
+const text = fs.readFileSync('index.html','utf8');
+//console.log(text);
+const http = require('http');
+const server = http.createServer(function(req,res){
+	const text = fs.readFileSync('index.html','utf8');
+	const style = fs.readFileSync('style.css','utf8');
+
+req.url=== '/style.css'? res.end(style):res.end(text);
+console.log(req.method, req.url);
+});	
+server.listen('3000');
